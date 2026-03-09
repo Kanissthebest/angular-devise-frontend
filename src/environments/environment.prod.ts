900c6e5dd66a6e3831ecc5b6d481045ec8604a2c
kanissthebest <issakane545@gmail.com>
2026-03-09 20:29:05 +0000

    mise à jour de l'url de render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://angular-devise-backend.onrender.com/devises',  // ⚠️ Remplacez par votre URL Render
+  apiUrl: 'https://angular-devise-backend.onrender.com',  // ⚠️ Remplacez par votre URL Render
   apiKey: 'eeeeecb8864aa6f18db9a1c1'  // API key pour exchangerate-api.com
 };
